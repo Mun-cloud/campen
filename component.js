2e455cd6ca85f442fb6ce474fdda64764ed6795f
Taeho <mun05170@naver.com>
2022-01-31 13:37:32 +0900

    팝업 페이지 자바스크립트 구현 완료

---
+++ component.js
@@ -0,0 +1,13 @@
+/** footer 사업자 정보 스크립트 */
+const businessInfo = document.querySelector(".business_info");
+const businessInfoIcon = document.querySelector(".business_info i");
+
+businessInfoIcon.addEventListener("click", () => {
+  if (businessInfo.style.height == "134px") {
+    businessInfoIcon.classList = "fas fa-chevron-down";
+    businessInfo.style.height = "30px";
+  } else {
+    businessInfoIcon.classList = "fas fa-chevron-up";
+    businessInfo.style.height = "134px";
+  }
+});
