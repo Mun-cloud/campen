86c723cfd55874e1fd6864e649dc63ebcd2f2c34
Taeho <mun05170@naver.com>
2022-03-07 16:38:48 +0900

    Contents 수정기능 추가 #17

--- backend/src/controllers/Contents.js
+++ backend/src/controllers/Contents.js
@@ -75,7 +75,7 @@ module.exports = (app) => {
   });
 
   /** 데이터 추가 --> Create(INSERT) */
-  router.post("/professor", async (req, res, next) => {
+  router.post("/content", async (req, res, next) => {
     if (!req.session.memberInfo) {
       return next(new BadRequestException("로그인중이 아닙니다."));
     }
@@ -130,32 +130,27 @@ module.exports = (app) => {
   });
 
   /** 데이터 수정 --> Update(UPDATE) */
-  router.put("/professor/:profno", async (req, res, next) => {
-    const profno = req.get("profno");
-    const name = req.post("name");
-    const userid = req.post("userid");
-    const position = req.post("position");
-    const sal = req.post("sal");
-    const hiredate = req.post("hiredate");
-    const comm = req.post("comm");
-    const deptno = req.post("deptno");
+  router.put("/content/:id", async (req, res, next) => {
+    id, tab, content, views, reg_date, edit_date, members_id, camp_id;
+    const id = req.get("id");
+    const content = req.post("content");
 
-    try {
-      regexHelper.value(name, "교수이름이 없습니다.");
-      regexHelper.value(userid, "아이디가 없습니다.");
-      regexHelper.value(position, "직급이 없습니다.");
-      regexHelper.value(sal, "급여가 없습니다.");
-      regexHelper.value(hiredate, "입사일 없습니다.");
-      regexHelper.value(deptno, "소속학과 번호가 없습니다.");
-      regexHelper.maxLength(name, 50, "이름이 너무 깁니다.");
-      regexHelper.maxLength(userid, 50, "아이디가 너무 깁니다.");
-      regexHelper.maxLength(position, 20, "직급이 너무 깁니다.");
-    } catch (err) {
-      return next(err);
-    }
+    // try {
+    //   regexHelper.value(name, "교수이름이 없습니다.");
+    //   regexHelper.value(userid, "아이디가 없습니다.");
+    //   regexHelper.value(position, "직급이 없습니다.");
+    //   regexHelper.value(sal, "급여가 없습니다.");
+    //   regexHelper.value(hiredate, "입사일 없습니다.");
+    //   regexHelper.value(deptno, "소속학과 번호가 없습니다.");
+    //   regexHelper.maxLength(name, 50, "이름이 너무 깁니다.");
+    //   regexHelper.maxLength(userid, 50, "아이디가 너무 깁니다.");
+    //   regexHelper.maxLength(position, 20, "직급이 너무 깁니다.");
+    // } catch (err) {
+    //   return next(err);
+    // }
 
     function foundNull() {
-      [profno, name, userid, position, sal, hiredate, deptno].forEach((v) => {
+      [id, content].forEach((v) => {
         if (v === null) {
           return false;
         }
@@ -176,18 +171,8 @@ module.exports = (app) => {
       await dbcon.connect();
 
       // 데이터 수정하기
-      const sql =
-        "UPDATE professor SET name=?, userid=?, position=?, sal=?, hiredate=?, comm=?, deptno=? WHERE profno=?";
-      const input_data = [
-        name,
-        userid,
-        position,
-        sal,
-        hiredate,
-        comm,
-        deptno,
-        profno,
-      ];
+      const sql = "UPDATE contents SET content=? WHERE id=?";
+      const input_data = [content, id];
       const [result1] = await dbcon.query(sql, input_data);
 
       // 결과 행 수가 0이라면 예외처리
@@ -196,8 +181,8 @@ module.exports = (app) => {
       }
 
       // 새로 저장된 데이터의 PK값을 활용하여 다시 조회
-      const sql2 = "SELECT * FROM professor WHERE profno=?";
-      const [result2] = await dbcon.query(sql2, [profno]);
+      const sql2 = "SELECT * FROM contents WHERE id=?";
+      const [result2] = await dbcon.query(sql2, [id]);
 
       // 조회 결과를 미리 준비한 변수에 저장함
       json = result2;
@@ -212,10 +197,10 @@ module.exports = (app) => {
   });
 
   /** 데이터 삭제 --> Delete(DELETE) */
-  router.delete("/professor/:profno", async (req, res, next) => {
-    const profno = req.get("profno");
+  router.delete("/content/:id", async (req, res, next) => {
+    const id = req.get("id");
 
-    if (profno === null) {
+    if (id === null) {
       return next(new Error(400));
     }
 
@@ -226,13 +211,13 @@ module.exports = (app) => {
       await dbcon.connect();
 
       // 자식데이터 null 주기
-      await dbcon.query("UPDATE student SET profno=null WHERE profno=?", [
-        profno,
-      ]);
+      // await dbcon.query("UPDATE student SET id=null WHERE id=?", [
+      //   id,
+      // ]);
 
       // 데이터 삭제하기
-      const sql = "DELETE FROM professor WHERE profno=?";
-      const [result1] = await dbcon.query(sql, [profno]);
+      const sql = "DELETE FROM contents WHERE id=?";
+      const [result1] = await dbcon.query(sql, [id]);
 
       json = result1;
       // 결과 행 수가 0이라면 예외처리
