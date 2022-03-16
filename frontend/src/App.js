bed2b46e9db4c8b107f8cbce6b779303f7664268
Taeho <mun05170@naver.com>
2022-03-16 21:36:03 +0900

    HTML 라우팅 #18

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,10 @@ import Index from "./pages/Index";
 import Search from "./pages/Search";
 import Camp from "./pages/Camp";
 import Exhibition from "./pages/Exhibition";
+import ExhiList from "./pages/ExhiList";
+import Heart from "./pages/Heart";
+import Join from "./pages/Join";
+import MyActive from "./pages/MyActive";
 
 function App() {
   return (
@@ -23,6 +27,10 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/search/:id" element={<Camp />} />
         <Route path="/exhibition" element={<Exhibition />} />
+        <Route path="/exhilist" element={<ExhiList />} />
+        <Route path="/heart" element={<Heart />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/my-active" element={<MyActive />} />
       </Routes>
 
       <Nav />
