3537d35df06cb3b0ed702308ca0ae99c08819db3
ssonnni <sohyun2638@gmail.com>
2022-03-20 23:42:52 +0900

    .

--- frontend/src/components/Write/WriteButton.js
+++ frontend/src/components/Write/WriteButton.js
@@ -1,5 +1,35 @@
 import style from "styled-components";
 
+const TabContainer = styled.ul`
+  max-width: 530px;
+  height: 45px;
+  width: 100%;
+  top: 0px;
+  background-color: rgb(255, 255, 255);
+  border-bottom: 1px solid rgb(212, 217, 214);
+  position: fixed;
+  display: flex;
+  z-index: 1000;
+`;
+
+const TabItem = styled.li`
+  display: inline-block;
+  height: 45px;
+  width: 25%;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    font-size: 10.5pt;
+    font-weight: 500;
+    color: #666;
+    cursor: pointer;
+  }
+`;
+
 const WriteButton = () => {
   return (
     <>
