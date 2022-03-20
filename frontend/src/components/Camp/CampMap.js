df6c3e3d49a7b5ddf87e8f61ee51d0fe661f68b0
Taeho <mun05170@naver.com>
2022-03-20 22:43:02 +0900

    상세페이지 캠핑장 배치도

--- frontend/src/components/Camp/CampMap.js
+++ frontend/src/components/Camp/CampMap.js
@@ -1,10 +1,26 @@
+import styled from "styled-components";
+
+const MapBox = styled.section`
+  img {
+    width: 100%;
+    height: fit-content;
+    border-radius: 10px;
+  }
+
+  .caption {
+    margin-top: 8px;
+    color: rgb(90, 94, 91);
+    font-size: 12px;
+  }
+`;
+
 const CampMap = () => {
   return (
-    <section className="camp_container" id="camp_map">
+    <MapBox className="camp_container" id="camp_map">
       <div className="box_title">캠핑존 배치도</div>
-      <img src="img/camp_map.jpg" alt="배치도" />
+      <img src={require("../../assets/img/camp_map.jpg")} alt="배치도" />
       <div className="caption">지도를 터치하여 크게 볼 수 있습니다.</div>
-    </section>
+    </MapBox>
   );
 };
 
