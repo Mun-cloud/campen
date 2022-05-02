2fea166332773531267f014e61594b6a9bfffda8
Taeho <mun05170@naver.com>
2022-05-02 16:12:44 +0900

    캠핑장 이미지 팝업

--- frontend/src/components/Camp/CampMap.js
+++ frontend/src/components/Camp/CampMap.js
@@ -5,6 +5,7 @@ const MapBox = styled.section`
     width: 100%;
     height: fit-content;
     border-radius: 10px;
+    cursor: pointer;
   }
 
   .caption {
@@ -15,14 +16,22 @@ const MapBox = styled.section`
 `;
 
 const CampMap = ({ item }) => {
+  const onClick = () => {
+    window.open(
+      item.photo,
+      "new",
+      "scrollbars=no,titlebar=no,status=no,resizable=no,fullscreen=no"
+    );
+  };
+
   if (item.photo === undefined) {
     return null;
   } else {
     return (
       <MapBox className="camp_container" id="camp_map">
-        <div className="box_title">캠핑존 배치도</div>
-        <img src={item.photo} alt="배치도" />
-        {/* <div className="caption">지도를 터치하여 크게 볼 수 있습니다.</div> */}
+        <div className="box_title">캠핑장 안내 이미지</div>
+        <img src={item.photo} alt="배치도" onClick={onClick} />
+        <div className="caption">지도를 터치하여 크게 볼 수 있습니다.</div>
       </MapBox>
     );
   }
