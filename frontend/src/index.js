da20fb2ac17b400a5d4ddc77f8ddc48624e37c28
Taeho <mun05170@naver.com>
2022-05-24 20:10:44 +0900

    theme setting #44

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,15 +6,29 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from "./store";
+import { ThemeProvider } from "styled-components";
 
 const queryClient = new QueryClient();
+
+const campen = {
+  white: "#fff",
+  mainColor: "rgb(67, 192, 131)",
+  darkMainColor: "rgb(34, 119, 88)",
+  darkDarkGreen: "rgba(62, 204, 121, 0.18)",
+  darkGreen: "rgba(34, 119, 88, 0.4)",
+  lightGray: "rgb(234, 238, 236)",
+  nomalGray: "rgb(212, 217, 214)",
+  darkGray: "rgb(90, 94, 91)",
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <App />
+          <ThemeProvider theme={campen}>
+            <App />
+          </ThemeProvider>
         </BrowserRouter>
       </QueryClientProvider>
     </Provider>
