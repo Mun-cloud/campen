81d19dbdcc63b1229166b1aa352ac56cbf9520b9
Taeho <mun05170@naver.com>
2022-05-28 00:28:59 +0900

    Update WriteButton.js

--- frontend/src/components/Write/WriteButton.js
+++ frontend/src/components/Write/WriteButton.js
@@ -34,7 +34,7 @@ const WriteButton = ({ btnText, postCommu }) => {
         disabled={!(btnText.length >= 10)}
         onClick={postCommu}
       >
-        10자 이상 입력해주세요.
+        {btnText.length >= 10 ? "작성 완료" : "10자 이상 입력해주세요."}
       </WriteSubmit>
     </>
   );
