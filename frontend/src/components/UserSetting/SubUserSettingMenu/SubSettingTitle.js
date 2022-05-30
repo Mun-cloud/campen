7daa125b3f3e44c36d20d2dd33fdf254ade9f5f0
Taeho <mun05170@naver.com>
2022-05-30 19:32:30 +0900

    코드정리 #44

---
+++ frontend/src/components/UserSetting/SubUserSettingMenu/SubSettingTitle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Title = styled.div`
+  margin: 18px 0px 43px;
+
+  div {
+    font-size: 16pt;
+    font-weight: 700;
+    margin-bottom: 8px;
+  }
+`;
+
+const SubSettingTitle = () => {
+  return (
+    <>
+      {/*  <!-- 타이틀--> */}
+      <Title />
+    </>
+  );
+};
+
+export default SubSettingTitle;
