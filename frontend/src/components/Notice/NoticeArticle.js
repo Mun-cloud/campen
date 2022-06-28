cc15015a3a04973a9358269bf93efcb12c036d52
Taeho <mun05170@naver.com>
2022-06-28 23:08:14 +0900

    Update NoticeArticle.js

--- frontend/src/components/Notice/NoticeArticle.js
+++ frontend/src/components/Notice/NoticeArticle.js
@@ -10,12 +10,15 @@ const Container = styled.div`
 `;
 
 const NoticeArticle = ({ data }) => {
+  // 파라미터 가져움
   const { id } = useParams();
+  // 파라미터에 맞는 공지사항 찾음
   const notice = data.item.find((v) => v.id === parseInt(id));
   return (
     <Container className="pop_main notice">
       <div className="notice_article_title">{notice.title}</div>
       <div className="notice_article_article">
+        {/* 본문 출력 */}
         {notice.content.split("\n").map((txt, i) => (
           <p key={i}>{txt}</p>
         ))}
