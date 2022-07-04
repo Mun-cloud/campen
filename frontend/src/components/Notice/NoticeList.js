308ecb9296f9a2c6433d14c428abad2f07d80f74
Taeho <mun05170@naver.com>
2022-07-04 11:15:25 +0900

    Update NoticeList.js

--- frontend/src/components/Notice/NoticeList.js
+++ frontend/src/components/Notice/NoticeList.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const NoticeList = ({ data }) => {
   // 보여질 공지사항 수(pagenation)
   const [viewMore, setViewMore] = useState(4);
+  // UTC 시간 변환 기능
+  const [reg, setReg] = useState();
+  useEffect(() => {
+    let dateObj = new Date(data.reg_date);
+    setReg(dateObj.toLocaleString("ko-KR", { timeZone: "Asia/Seoul" }));
+  }, [data]);
   return (
     <>
       <div className="pop_main">
@@ -15,7 +21,7 @@ const NoticeList = ({ data }) => {
               <div className="notice_box" data-notice="notice-1.html">
                 <div className="notice_info">
                   <div className="notice_title">{v.title}</div>
-                  <div className="notice_date">{v.reg_date}</div>
+                  <div className="notice_date">{reg}</div>
                 </div>
                 <div className="notice_chev">
                   <i className="fas fa-chevron-right"></i>
